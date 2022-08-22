5c24cb35c6c0f50aad9071dbe9124a9cd20fb57d
kutayerten <kutayerten@gmail.com>
2022-08-22 15:57:36 +0300

    added styled-components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,7 +10,7 @@ import Button from '../button/button.component';
 
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import {CartDropdownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles';
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
@@ -22,14 +22,20 @@ const CartDropdown = () => {
 
 
     return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items' >
+        <CartDropdownContainer>
+            <CartItems >
+            {cartItems.length ? (cartItems.map((item) => (
+                    <CartItem key={item.id} cartItem={item}/>
+                ))) : (
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )}
+
             {cartItems.map((item) => (
             <CartItem key={item.id} cartItem={item} />
             ))}
-            </div>
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     );
 };
 
